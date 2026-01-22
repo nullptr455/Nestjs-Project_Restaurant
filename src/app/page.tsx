d4ef485d7692ed1aecdcb1886767a1d8fab6fb72
_KashifAsif <kashifasif.cs@gmail.com>
2026-01-22 23:10:44 +0500

    Navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,15 @@
+import Featured from "@/component/Featured";
+import Offer from "@/component/Offer";
+import Slider from "@/component/Slider";
 
 
 export default function Home() {
   return (
-    <main>helo world</main>
+    <main>
+      <Slider/>
+      <Featured/>
+      <Offer/>
+    </main>
   );
 }
 
--- src/component/Footer.tsx
+++ src/component/Footer.tsx
@@ -1,8 +1,12 @@
+import Link from "next/link";
 import React from "react";
 
 const Footer = () =>{
     return(
-        <div className="">Footer</div>
+        <div className="h-12 md:h-24 p-4 lg:p-20 xl:p-40 text-red-500 flex items-center justify-between">
+            <Link href="/" className="font-b">MASSIMO</Link>
+            <p>O ALL RIGHT RESERVED</p>
+        </div>
     );
 }
 
