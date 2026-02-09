c71bf341d1a38c9786da192d60a4bce2525be02f
_KashifAsif <kashifasif.cs@gmail.com>
2026-02-09 07:50:56 +0500

    featured

--- src/component/Featured.tsx
+++ src/component/Featured.tsx
@@ -11,26 +11,27 @@ const Featured = () => {
         {featuredProducts.map((item) => (
           <div
             key={item.id}
-            className="w-screen h-[60vh] flex flex-col items-center justify-around p-4"
+            className="w-screen h-[60vh] flex flex-col items-center justify-around p-4 hover:bg-fuchsia-50 transition-all duration-300 md:w-[50vw] xl:w-[33vw] xl:h-[90vh]"
           >
             {/* IMAGE CONTAINNER */}
             {item.img && (
-              <div className="relative w-full h-[250px] md:h-[300px]">
+              <div className="relative flex-1 w-full hover:rotate-[60deg] transition-all duration-500">
                 <Image
                   src={item.img}
                   alt={item.title}
                   fill
                   className="object-contain"
-                  sizes="(max-width: 768px) 100vw, 50vw"
                 />
               </div>
             )}
             {/* Text containner */}
-            <div className="flex-1 flex flex-col gap-4">
-              <h1 className="text-xl font-bold uppercase">{item.title}</h1>
-              <p>{item.desc}</p>
+            <div className="flex-1 flex flex-col items-center justify-center text-center gap-4">
+              <h1 className="text-xl font-bold uppercase xl:text-2xl 2xl:text-3xl">
+                {item.title}
+              </h1>
+              <p className="p-4 2xl:p-8">{item.desc}</p>
               <span className="text-xl font-bold">{item.price}</span>
-              <button className="bg-red-500 text-white p-2 rounded-md">
+              <button className="bg-red-500 text-white p-2 rounded-md cursor-pointer">
                 Add to Cart
               </button>
             </div>
