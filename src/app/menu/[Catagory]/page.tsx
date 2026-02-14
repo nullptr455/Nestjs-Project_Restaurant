17278f4d019cb17377cf0067bf8ae70b829694ff
_KashifAsif <kashifasif.cs@gmail.com>
2026-02-15 00:52:11 +0500

    menu

--- src/app/menu/[Catagory]/page.tsx
+++ src/app/menu/[Catagory]/page.tsx
@@ -1,9 +1,35 @@
 import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { pizzas } from "@/component/data";
 
-const CategoryPage = () =>{
-    return(
-        <div>CategoryPage</div>
-    );
-}
+const CategoryPage = () => {
+  return (
+    <div className="flex flex-wrap text-red-500">
+      {pizzas.map((item) => (
+        <Link
+          className="w-full h-[60vh] border-r-2 border-b-2 border-red-500 sm:w-1/2 lg:w-1/3 p-4 flex flex-col justify-between group "
+          href={`/product/${item.id}`}
+          key={item.id}
+        >
+          {/* Image container */}
+          {item.img && (
+            <div className="relative h-[80%]">
+              <Image src={item.img} alt="" fill className="object-contain" />
+            </div>
+          )}
+          {/* text container */}
+          <div className="flex items-center justify-between font-bold">
+            <h1 className="text-2xl uppercase p-2">{item.title}</h1>
+            <h2 className="group-hover:hidden text-xl">${item.price}</h2>
+            <button className="hidden group-hover:block uppercase bg-red-500 text-white p-2 rounded-md">
+              Add to Cart
+            </button>
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
+};
 
-export default CategoryPage
+export default CategoryPage;
