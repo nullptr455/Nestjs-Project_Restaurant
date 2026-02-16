8de28b3857ef71231c3d8a2ca450ca896816d024
_KashifAsif <kashifasif.cs@gmail.com>
2026-02-17 00:23:15 +0500

    order

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -8,13 +8,13 @@ const Loginpage = () => {
       <div className="h-full shadow-2xl rounded-md flex flex-col md:flex-row md:h-[70%] md:w-full lg:w-[60%] 2xl:w-1/2">
         {/* image contain */}
         <div className="relative h-1/3 w-full md:h-full md:w-1/2">
-          <Image src="/loginBg.png" alt="" fill className="object-contain" />
+          <Image src="/loginBg.png" alt="" fill className="object-cover" />
         </div>
         {/* form container */}
         <div className="p-10 flex flex-col gap-8 md:w-1/2">
           <h1 className="font-bold text-xl">Welcome</h1>
           <p>Log into your account or create a new one using social buttons</p>
-          <button className="flex gap-4 p-4 ring-1 ring-orange-100 rounded-md">
+          <button className="flex gap-4 p-4 ring-1 ring-orange-100 rounded-md cursor-pointer">
             <Image
               src="/google.png"
               alt=""
@@ -24,7 +24,7 @@ const Loginpage = () => {
             />
             <span>Sign in with Google</span>
           </button>
-          <button className="flex gap-4 p-4 ring-1 ring-orange-100 rounded-md">
+          <button className="flex gap-4 p-4 ring-1 ring-orange-100 rounded-md cursor-pointer">
             <Image
               src="/facebook.png"
               alt=""
--- src/component/Footer.tsx
+++ src/component/Footer.tsx
@@ -1,13 +1,15 @@
 import Link from "next/link";
 import React from "react";
 
-const Footer = () =>{
-    return(
-        <div className="h-12 md:h-24 p-4 lg:p-20 xl:p-40 text-red-500 flex items-center justify-between">
-            <Link href="/" className="font-b">MASSIMO</Link>
-            <p>O ALL RIGHT RESERVED</p>
-        </div>
-    );
-}
+const Footer = () => {
+  return (
+    <div className="h-12 md:h-24 p-4 lg:px-20 xl:px-40 text-red-500 flex items-center justify-between">
+      <Link href="/" className="font-b">
+        MASSIMO
+      </Link>
+      <p>O ALL RIGHT RESERVED</p>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
